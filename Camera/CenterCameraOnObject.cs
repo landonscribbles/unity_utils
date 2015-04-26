663017e9bed4c9615ff979f9c539b9646afffdc9
Landon <landonscribbles@gmail.com>
2015-04-26 15:27:40 -0700

    Added camera follow on gameobject

---
+++ Camera/CenterCameraOnObject.cs
@@ -0,0 +1,24 @@
+﻿/* Info: Have the main camera follow the player on the X and Y axis.
+ * Was tested with an Orthographic camera
+ * 
+ * Tested on:
+ * Winders
+ * 
+ * Known Issues:
+ * None (yet)
+ * 
+*/
+using UnityEngine;
+using System.Collections;
+
+public class CenterCameraOnObject : MonoBehaviour {
+    public GameObject followedObject;
+
+	void Update () {
+        Vector3 curretCameraLoc = Camera.main.transform.position;
+        Vector3 newCameraLoc = new Vector3(
+            followedObject.transform.position.x, followedObject.transform.position.y, curretCameraLoc.z
+        );
+        Camera.main.transform.position = newCameraLoc;
+	}
+}
